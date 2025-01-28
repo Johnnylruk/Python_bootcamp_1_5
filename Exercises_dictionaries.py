bd28e0038a15aaf8eb2c2b6ffc27276a2d0914e3
Johnny Rodrigues <johnny.rodrigues@fdmgroup.com>
2025-01-28 15:26:32 +0000

    Dictionary exercises finished

--- Exercises_dictionaries.py
+++ Exercises_dictionaries.py
@@ -87,43 +87,83 @@
 # for match in range(player['Matches']):
 #         print(f'   => Match {match + 1}, scored {scores[match]}')    
 
-num = 0
-data = dict()
-list_data = list()
-count = 0
-age_sum = 0
-woman_list= list()
-above_average_age = list()
+# num = 0
+# data = dict()
+# list_data = list()
+# count = 0
+# age_sum = 0
+# woman_list= list()
+# above_average_age = list()
 
-while True:
-    data['Name'] = str(input('Name: '))
-    data['Age'] = int(input('Age: '))
-    data['Gender'] = str(input('Gender [M/F]: ')).upper()
-    list_data.append(data.copy())
-    answer = str(input("Do you want to continue? [Y/N]"))
-    if answer in 'Nn':
-        for people in list_data:
-            count += 1
-        print(f"{count} were sucesfull registered")
-        for dt in list_data:
-            for k, v in dt.items():
-                if k == 'Age':
-                    age_sum += v
-                elif v == 'F':
-                    woman_list.append(dt['Name'])
-        mean = age_sum / count
-        print(f'The age mean is: {mean}')
-        for person in list_data:
-            for k, v in person.items():
-                if k == 'Age':
-                    if v > mean:
-                        if v not in above_average_age:
-                            above_average_age.append(person.copy())
-        if len(woman_list) != 0:     
-            print(f"This/These are the woman registered: {woman_list}")
-        if len(above_average_age) != 0:
-            for person in above_average_age:
-                for k, v in person.items():
-                    print(f'{k} = {v}', end='; ')
-        break
+# while True:
+#     data['Name'] = str(input('Name: '))
+#     data['Age'] = int(input('Age: '))
+#     data['Gender'] = str(input('Gender [M/F]: ')).upper()
+#     list_data.append(data.copy())
+#     answer = str(input("Do you want to continue? [Y/N]"))
+#     if answer in 'Nn':
+#         for people in list_data:
+#             count += 1
+#         print(f"{count} were sucesfull registered")
+#         for dt in list_data:
+#             for k, v in dt.items():
+#                 if k == 'Age':
+#                     age_sum += v
+#                 elif v == 'F':
+#                     woman_list.append(dt['Name'])
+#         mean = age_sum / count
+#         print(f'The age mean is: {mean}')
+#         for person in list_data:
+#             for k, v in person.items():
+#                 if k == 'Age':
+#                     if v > mean:
+#                         if v not in above_average_age:
+#                             above_average_age.append(person.copy())
+#         if len(woman_list) != 0:     
+#             print(f"This/These are the woman registered: {woman_list}")
+#         if len(above_average_age) != 0:
+#             for person in above_average_age:
+#                 for k, v in person.items():
+#                     print(f'{k} = {v}', end='; ')
+#         break
             
+
+
+scores = []
+total_score = 0
+list_players = list()
+players = {}
+positions = []
+while True:
+    players['Name'] = str(input("Name: "))
+    players['Matches'] = int(input("How many matches: "))
+
+    for score in range(players['Matches']):
+        scores.append(int(input(f"How many score on game{score + 1}: ")))
+    players['Scores'] = scores[:]
+    players['Total_score'] = sum(scores)
+    list_players.append(players.copy())
+    del players
+    scores.clear()
+    players = {}
+    answer = str(input("Do you wish to continue? "))
+    if answer in 'Nn':
+        print('-=' * 40)
+        for pos, value in enumerate(list_players):
+            print(pos, value['Name'], value['Scores'], value['Total_score'])
+            positions.append(pos)
+        display = int(input("Which player data do you want to display? (999 to exit()) "))
+        while display != 999:
+            if display not in positions:
+                print("Error! Player does not exist.")
+                display = int(input("Which player data do you want to display? (999 to exit()) "))
+            else:
+                print('-=' * 40)
+                print(f"-- Player {list_players[display]['Name']} data:")
+                print(f"Player {list_players[display]['Name']} played {list_players[display]['Matches']} matches")
+                for match in range(list_players[display]['Matches']):
+                     print(f'   => Match {match + 1}, scored {list_players[display]['Scores'][match]}') 
+                display = int(input("Which player data do you want to display? (999 to exit()) "))
+        break
+
+   
